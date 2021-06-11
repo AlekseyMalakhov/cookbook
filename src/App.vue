b42eb784a236cca75c8703f6c813f799c59c9496
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-06-11 16:39:21 +0300

    add header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <Header />
-  <Home />
+
+  <el-container>
+    <el-header>
+      <Header />
+    </el-header>
+
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main><Home /></el-main>
+    </el-container>
+
+    <el-footer>Footer</el-footer>
+  </el-container>
+  
 </template>
 
 <script>
@@ -21,4 +33,30 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+
+.el-header, .el-footer {
+  background-color: #409EFF;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,45 @@
 <template>
-    <el-menu mode="horizontal" background-color="#409EFF">
-        <el-menu-item index="1">Cookbook</el-menu-item>
-
-    </el-menu>
+    <el-row type="flex" justify="space-between" align="middle">
+        <el-col :span="12" >
+            <el-row type="flex" align="middle" justify="start" class="title">
+                Cookbook
+            </el-row>
+        </el-col>
+        <el-col :span="12">
+            <el-row type="flex" align="middle" justify="end">
+                <el-avatar :size="50" :src="state.circleUrl"></el-avatar>
+            </el-row>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
+import { reactive } from "vue";
+
 export default {
-    name: "Header"
+    name: "Header",
+    setup() {
+        const state = reactive({
+            circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
+        });
+
+        return {
+            state,
+        };
+    },
+
 }
 </script>
 
 <style lang="scss" scoped>
-    .el-menu-item {
+    
+    .title {
         color: white;
         font-size: 20px;
     }
+
+
+   
+
+
 </style>

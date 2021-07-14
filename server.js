03bbe66e0cd5276c7985418356a26319e98c2db3
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-07-14 10:42:39 +0300

    remove password from users list

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
@@ -9,10 +9,11 @@ app.use(express.json());
 //login - megauser
 //pass - 123456789qqq
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://megauser:123456789qqq@cluster0.hsrcs.mongodb.net/cookbook?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
+const uri =
+    "mongodb+srv://megauser:123456789qqq@cluster0.hsrcs.mongodb.net/cookbook?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 //connect to DB
@@ -30,40 +31,39 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 //create account
-app.post('/create_account', (req, res) => {
+app.post("/create_account", (req, res) => {
     const newAccount = req.body;
 
     //check if username is free
     async function checkForExisting() {
         const query = {
             name: newAccount.name,
-        }
+        };
         const result = await collection.findOne(query);
         return result;
     }
-    checkForExisting().then((result) => {
+    checkForExisting().then(result => {
         if (!result) {
             createAccount();
         } else {
             res.sendStatus(409);
         }
-    });    
+    });
 
     async function createAccount() {
         const result = await collection.insertOne(newAccount);
-        res.send(result);    
+        res.send(result);
     }
-})
+});
 
 //login
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
     const user = req.body;
     async function getUser() {
         const query = {
             name: user.name,
-        }
+        };
         const result = await collection.findOne(query);
         console.log(result);
         if (!result) {
@@ -71,19 +71,23 @@ app.post('/login', (req, res) => {
             return;
         }
         if (result && result.password === user.password) {
-            res.send(result); 
+            res.send(result);
         } else {
             res.sendStatus(403);
         }
     }
     getUser();
-})
+});
 
 //get all users
 app.get("/users", (req, res) => {
     async function getAllUsers() {
         const cursor = await collection.find();
-        const result = await cursor.toArray();
+        const arr = await cursor.toArray();
+        const result = arr.map(user => {
+            delete user.password;
+            return user;
+        });
         if (!result) {
             res.sendStatus(403);
             return;
@@ -94,12 +98,7 @@ app.get("/users", (req, res) => {
     getAllUsers();
 });
 
-
-
-
-
 //start the server
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
-
+    console.log(`Example app listening at http://localhost:${port}`);
+});
